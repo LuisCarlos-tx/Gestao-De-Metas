96155ea93585a26ad270e797e2019319cf1a7520
Luis Carlos <luiscarlostvx34@gmail.com>
2025-11-23 21:25:14 -0300

    gestaoHabitos.java

---
+++ gestaoHabitos.java
@@ -0,0 +1,11 @@
+package com.gestaohabitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoHabitosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoHabitosApplication.class, args);
+    }
+}
